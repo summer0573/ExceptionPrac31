39dfa07257bda7b97eca44a522ed89f881e1f6f5
hyunjin <s2113@e-mirim.hs.kr>
2023-08-24 14:34:06 +0900

    초기 커밋.

---
+++ ExceptionPrac31/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPrac31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "가", "나" };
+            Console.Write("숫자를 입력해주세요 : ");
+
+                try {
+                    int input = int.Parse(Console.ReadLine());
+                    Console.WriteLine("입력한 위치의 값은 '" + array[input] + "'입니다.");
+                } catch(IndexOutOfRangeException ex) { 
+                    Console.WriteLine("올바른 인덱스를 입력하세요.");
+                } catch (FormatException ex) {
+                    Console.WriteLine("숫자를 입력하세요 ");
+                } catch(Exception ex) {
+                    Console.WriteLine(ex.GetType());
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                } finally { Console.WriteLine("옳게 실행되든 예외가 발생"); }
+        }
+    }
+}
